ec4ca8cddfe975b147c78cc459a6d465cabe04ec
coldly57 <coilnovagg@mail.ru>
2015-12-11 12:14:25 +0200

    Create MusicShop.java

---
+++ JavaCore/Module_2/MusicalInstrument/MusicShop.java
@@ -0,0 +1,11 @@
+public class MusicShop{
+    private Guitar guitar;
+    private Piano piano;
+    private Trumpet trumpet;
+
+    public MusicShop(Guitar guitar, Piano piano, Trumpet trumpet){
+        this.guitar = guitar;
+        this.piano = piano;
+        this.trumpet = trumpet;
+    }
+}
