11f6d2b3cc95c129db1eff8cca52a280a8fbde4b
coldly57 <coilnovagg@mail.ru>
2015-12-11 12:12:02 +0200

    Create MusicalInstrument.java

---
+++ JavaCore/Module_2/MusicalInstrument/MusicalInstrument.java
@@ -0,0 +1,7 @@
+public class MusicalInstrument
+{
+    public static void main(String[] args)
+    {
+        MusicShop musicShop = new MusicShop(new Guitar(), new Piano(), new Trumpet());
+    }
+}
