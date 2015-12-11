baa416f4517ff87071a9d3b50e9d1ba3fec3cd4c
coldly57 <coilnovagg@mail.ru>
2015-12-11 12:08:57 +0200

    Create Bouquet.java

---
+++ JavaCore/Module_2/Flower/Bouquet.java
@@ -0,0 +1,11 @@
+public class Bouquet{
+    private Rose rose;
+    private Chamomile chamomile;
+    private Aster aster;
+
+    public Bouquet(Rose rose, Chamomile chamomile, Aster aster){
+        this.rose = rose;
+        this.chamomile = chamomile;
+        this.aster = aster;
+    }
+}
