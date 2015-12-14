037932c26a1d1d9e7f3c285520b39a444b04d124
coldly57 <coilnovagg@mail.ru>
2015-12-14 14:09:25 +0200

    Update AudioFile.java

--- JavaCore/Module_2/TaskFile/AudioFile.java
+++ JavaCore/Module_2/TaskFile/AudioFile.java
@@ -1,3 +1,11 @@
-public class AudioFile extends File{
+package module2.task1file;
 
+public class AudioFile extends File {
+
+    private String currentString = "AudioFile";
+
+    @Override
+    public String getCurrentString(){
+        return this.currentString;
+    }
 }
