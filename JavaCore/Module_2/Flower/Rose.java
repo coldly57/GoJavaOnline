8fdb42fc63bc773eb9c0aadcbcc80acbbe5e2e7e
coldly57 <coilnovagg@mail.ru>
2015-12-14 14:15:08 +0200

    Update Rose.java

--- JavaCore/Module_2/Flower/Rose.java
+++ JavaCore/Module_2/Flower/Rose.java
@@ -1,3 +1,11 @@
-public class Rose extends Flower{
+package module2.task2flower;
 
+public class Rose extends Flower {
+
+    private String currentString = "Rose";
+
+    @Override
+    public String getCurrentString(){
+        return this.currentString;
+    }
 }
