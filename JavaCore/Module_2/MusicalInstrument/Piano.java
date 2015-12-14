3dcb48f8d7ea0a7e5cb7325c3b43c9f5c84aecb5
coldly57 <coilnovagg@mail.ru>
2015-12-14 14:18:45 +0200

    Update Piano.java

--- JavaCore/Module_2/MusicalInstrument/Piano.java
+++ JavaCore/Module_2/MusicalInstrument/Piano.java
@@ -1,3 +1,11 @@
-public class Piano extends MusicalInstrument{
+package module2.task3musicalinstrument;
 
+public class Piano extends MusicalInstrument {
+
+    private String currentString = "Piano";
+
+    @Override
+    public String getCurrentString(){
+        return this.currentString;
+    }
 }
