33b8efd5699218c9b41d7b1e035ddda113b9ed62
coldly57 <coilnovagg@mail.ru>
2015-12-14 14:16:51 +0200

    Update Tulip.java

--- JavaCore/Module_2/Flower/Tulip.java
+++ JavaCore/Module_2/Flower/Tulip.java
@@ -1,3 +1,11 @@
-public class Tulip extends Flower{
+package module2.task2flower;
 
+public class Tulip extends Flower {
+
+    private String currentString = "Tulip";
+
+    @Override
+    public String getCurrentString(){
+        return this.currentString;
+    }
 }
