c93c76adb62261d6301a817fd9b73e6ad064505c
coldly57 <coilnovagg@mail.ru>
2015-12-14 14:12:53 +0200

    Update Aster.java

--- JavaCore/Module_2/Flower/Aster.java
+++ JavaCore/Module_2/Flower/Aster.java
@@ -1,3 +1,11 @@
-public class Aster extends Flower{
+package module2.task2flower;
 
+public class Aster extends Flower {
+
+    private String currentString = "Aster";
+
+    @Override
+    public String getCurrentString(){
+        return this.currentString;
+    }
 }
