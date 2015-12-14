7d41a5346eb3910784e97e50afabbc5d1905908b
coldly57 <coilnovagg@mail.ru>
2015-12-14 14:16:26 +0200

    Update Rosebush.java

--- JavaCore/Module_2/Flower/Rosebush.java
+++ JavaCore/Module_2/Flower/Rosebush.java
@@ -1,3 +1,11 @@
-public class Rosebush extends Flower{
+package module2.task2flower;
 
+public class Rosebush extends Flower {
+
+    private String currentString = "Rosebush";
+
+    @Override
+    public String getCurrentString(){
+        return this.currentString;
+    }
 }
