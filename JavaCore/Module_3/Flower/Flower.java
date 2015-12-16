cdfc117f85f3d11cfa27933d040cfeffab2e003b
coldly57 <coilnovagg@mail.ru>
2015-12-16 12:55:18 +0200

    Update Flower.java

--- JavaCore/Module_3/Flower/Flower.java
+++ JavaCore/Module_3/Flower/Flower.java
@@ -1,19 +1,8 @@
 package module3.task2flower;
 
-public class Flower {
+public abstract class Flower {
 
     private String currentString = "Flower";
-
-    public static void main(String[] args) {
-
-        Bouquet bouquet = new Bouquet(new Rose(), new Chamomile(), new Aster());
-
-        bouquet.setFlowersInBouquet(new Tulip(), new Rosebush());
-
-        for (Flower f : bouquet.getFlowersList()){
-            System.out.println(f.getCurrentString());
-        }
-    }
 
     public String getCurrentString(){
         return this.currentString;
